881593b68a31c5b23f37fbe90664167d89967921
Nguyen Kim Duong <nd135669@gmail.com>
2025-01-11 08:32:30 +0700

    Khởi tạo dự án

---
+++ Lab_1_PRN222/ServerApp/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+class Program
+{
+    static void ExcuteServer(string host, int port)
+    {
+        int Count = 0;
+        TcpListener server = null;
+        try
+        {
+            Console.Title = "Server Application";
+            IPAddress localAddr = IPAddress.Parse(host);
+            server = new TcpListener(localAddr, port);
+            server.Start();
+
+            Console.WriteLine(new string('*', 40));
+            Console.WriteLine("Waiting for a connection ...");
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine($"Number of client connected: {++Count}");
+                Console.WriteLine(new string('*', 40));
+                Thread thread = new Thread(new ParameterizedThreadStart(ProcessMessage));
+                thread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: {0}", ex.Message);
+        }
+        finally
+        {
+            server.Stop();
+            Console.WriteLine("Server stopped.");
+        }
+        Console.Read();
+    }
+
+    static void ProcessMessage(object parm)
+    {
+        string data;
+        int count;
+        try
+        {
+            TcpClient client = parm as TcpClient;
+            Byte[] bytes = new Byte[256];
+
+            NetworkStream stream = client.GetStream();
+
+            while ((count = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, count);
+                Console.WriteLine($"Received: {data} at {DateTime.Now:t}");
+                data = $"{data.ToUpper()}";
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+                Console.WriteLine($"Sent: {data}");
+            }
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Exception: {0}", ex.Message);
+            Console.WriteLine("Waiting message ...");
+        }
+
+    }
+    public static void Main()
+    {
+        string host = "127.0.0.1";
+        int port = 8080;
+        ExcuteServer(host, port);
+    }
+}
